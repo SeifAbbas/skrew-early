69c0ff07d475558c66cd3171075050eb5af78515
yousefyasser <yousefyasser6@gmail.com>
2024-04-26 02:14:30 +0300

    Added SignUp template, added signUp validation

--- src/Admin/AdminDashboard.js
+++ src/Admin/AdminDashboard.js
@@ -1,7 +1,7 @@
 const AdminDashboard = () => {
   return (
     <>
-      <h1>hello from organization Dashboard</h1>
+      <h1>hello from Admin Dashboard</h1>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import AdminDashboard from "./Admin/AdminDashboard";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AdminDashboard /> */}
     <App />
   </React.StrictMode>
 );
