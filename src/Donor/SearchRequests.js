0f8b975a94bdf35a63a9ee0db0d5d4332c0708df
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2024-04-28 23:40:30 +0300

    added filtering catgeory input feature

--- src/Donor/SearchRequests.js
+++ src/Donor/SearchRequests.js
@@ -3,26 +3,58 @@ import CardActions from '@mui/material/CardActions';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import TextField  from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 import dummyData from "../dummyData.json";
 
 export default function AlignItemsList() {
   const [searchTerm, setSearchTerm] = React.useState('');
-
+  const [category, setCategory] = React.useState([]);
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+  const categories = ['Clothes', 'Toys', 'Books','Medication','Medical Supplies','School Supplies','Food','Blood Donations', 'Teaching Classes', 'Doctor Visits'];
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const filteredItems = dummyData.requests.filter(item =>
+  const handleFilterChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setCategory(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+  let filteredItems = dummyData.requests.filter(item =>
     item.Category.toLowerCase().includes(searchTerm.toLowerCase())
   );
+  if (category.length > 0) {
+    filteredItems = filteredItems.filter(item =>
+      category.includes(item.Category)
+    );
+  }
   return (
     <div>
+    <Grid item xs={12} sm={6}>
     <TextField
         label= "Search"
         variant="outlined"
@@ -36,6 +68,29 @@ export default function AlignItemsList() {
           backgroundColor:'background.paper',
         }}
       />
+      <FormControl sx={{ m: 1, width: 200, backgroundColor:'background.paper', marginTop:'10px',
+          marginBottom:'10px',
+          marginLeft: '10px', }}>
+        <InputLabel id="demo-multiple-checkbox-label">Category</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={category}
+          onChange={handleFilterChange}
+          input={<OutlinedInput label="Category" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {categories.map((name) => (
+            <MenuItem key={name} value={name}>
+              <Checkbox checked={category.indexOf(name) > -1} />
+              <ListItemText primary={name} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      </Grid>
     <div className="list-container">
     <List sx={{ maxwidth: "20%", bgcolor: 'background.paper', display: 'flex', justifyContent: "space-between", flexWrap: 'wrap'  }}>
     {filteredItems.map((item, index) => (
