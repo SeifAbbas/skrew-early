93cd096701043563baee1465f389b7a8a26230ff
yousefyasser <yousefyasser6@gmail.com>
2024-04-29 23:50:46 +0300

    Added Categories Pie chart

--- src/Organization/OrganizationDashboard.js
+++ src/Organization/OrganizationDashboard.js
@@ -1,17 +1,16 @@
 import * as React from "react";
 import {
-  useTheme,
   Box,
   CssBaseline,
   Toolbar,
   Container,
   Grid,
   Paper,
+  Typography
 } from "@mui/material";
-import { LineChart } from "@mui/x-charts";
+import { LineChart, PieChart } from "@mui/x-charts";
 
 const Chart = () => {
-  const theme = useTheme();
   const xAxis = [
     "Jan",
     "Feb",
@@ -59,11 +58,34 @@ const Chart = () => {
                 value == null ? "NaN" : value.toString(),
             },
           ]}
-          height={300}
+          height={325}
         />
       </div>
     </>
   );
+};
+
+const CategoryChart = () => {
+  return (
+    <>
+    <Typography variant="h6" component="div" align="center">
+    Donation Request Categories
+    </Typography>  
+    <PieChart
+    series={[
+      {
+        data: [
+          { id: 0, value: 10, label: 'Food' },
+          { id: 1, value: 20, label: 'Medical Supplies' },
+          { id: 2, value: 45, label: 'Medication' },
+          { id: 3, value: 25, label: 'Clothes' },
+        ],
+      },
+    ]}
+    height={300}
+  />
+  </>
+)
 };
 
 const OrganizationDashboard = () => {
@@ -84,10 +106,10 @@ const OrganizationDashboard = () => {
           }}
         >
           <Toolbar />
-          <Container maxWidth="xl" sx={{ mt: -8, mb: 4, mx: 8 }}>
+          <Container maxWidth="xl" sx={{ mt: -8, mb: 4, mx: 15 }}>
             <Grid container spacing={3}>
               {/* Chart */}
-              <Grid item lg={7}>
+              <Grid item lg={6} md={12}>
                 <Paper
                   sx={{
                     p: 2,
@@ -96,6 +118,17 @@ const OrganizationDashboard = () => {
                   }}
                 >
                   <Chart />
+                </Paper>
+              </Grid>
+              <Grid item lg={5} md={12}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CategoryChart/>
                 </Paper>
               </Grid>
             </Grid>
