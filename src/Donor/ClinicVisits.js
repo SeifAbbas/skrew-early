c1fc959c3d1457afc7d06e56818135d664f2b9cf
Mohamed Ahmed El Sawy <m.elsawy2003@gmail.com>
2024-04-29 18:11:04 +0300

    edits in css

--- src/Donor/ClinicVisits.js
+++ src/Donor/ClinicVisits.js
@@ -5,28 +5,20 @@ import { Link } from 'react-router-dom';
 const ClinicVisits = () => {
     return ( 
         <div className="clinic-visits">
-            <h1>Address</h1>
             <input type="text" placeholder="Address" />
 
-            <h1>Area</h1>
             <input type="text" placeholder="Area" />
 
-            <h1>Governorate</h1>
             <input type="text" placeholder="Governorate" />
 
-            <h1>Specialty</h1>
             <input type="text" placeholder="Specialty" />
 
-            <h1>Number of casses</h1>
             <input type="number" placeholder="Number of casses" />
 
-            <h1>Date</h1>
             <input type="date" placeholder="Date" />
 
-            <h1>Available from</h1>
             <input type="time" placeholder=" Time" />
 
-            <h1>Available to</h1>
             <input type="time" placeholder=" Time" />
 
             <Link to="/Home/Dashboard">
--- src/Donor/TeachingClasses.js
+++ src/Donor/TeachingClasses.js
@@ -5,25 +5,18 @@ import { Link } from 'react-router-dom';
 const TeachingClasses = () => {
     return ( 
         <div className="teaching-class">
-            <h1>Subject</h1>
             <input type="text" placeholder="Subject" />
 
-            <h1>Study Year</h1>
             <input type="text" placeholder="Study Year" />
 
-            <h1>Minimum number of students to attend</h1>
             <input type="number" placeholder="Minimum number of students to attend" />
 
-            <h1>Maximum number of students to attend</h1>
             <input type="number" placeholder="Maximum number of students to attend" />
 
-            <h1>Class Duration in Hours</h1>
             <input type="number" placeholder="Class Duration" />
 
-            <h1>Date</h1>
             <input type="date" placeholder="Date" />
 
-            <h1>Class Time</h1>
             <input type="time" placeholder="Class Time" />
 
             <Link to="/Home/Dashboard">
