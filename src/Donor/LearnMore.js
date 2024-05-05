27d0eea9375a2569eca568c126ed5f50e8b3d355
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2024-05-05 18:37:59 +0300

    Customized learn more button and added donate button

--- src/Donor/LearnMore.js
+++ src/Donor/LearnMore.js
@@ -1,32 +1,36 @@
 import dummyData from "../dummyData.json";
 import { Link, useParams } from "react-router-dom";
-import { Divider } from "@mui/material";
+import { Divider, Button, Box } from "@mui/material";
 
 const LearnMore = () => {
   const { id } = useParams();
   const idNum = Number(id);
 
   return (
-    <div className="learn-more">
+    <div className="learn-more" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       {dummyData.requests.map(
         (item) =>
           item.ID === idNum && (
-            <>
-              <div className="learn-more-container">
-                <img src={item.ImageSrc} alt="" />
-                <h1>{item.Name}</h1>
-                <h1>{item.Category}</h1>
-                <h1>{item.Current_Inventory}</h1>
-                <h1>{item.Gender || item.Type}</h1>
-                <h1>{item.Age || item.Season}</h1>
-              </div>
-              <Divider component="li" />
-            </>
+            <div key={item.ID} className="learn-more-container" style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
+              <img src={item.ImageSrc} alt="" style={{ width: '100%', maxWidth: '300px', height: 'auto', marginLeft: "10px" }} />
+              <h1>{item.Name}</h1>
+              {item.Category && <h1>{item.Category}</h1>}
+              {item.Current_Inventory && <h1>{"Quantity Needed: " + item.Current_Inventory}</h1>}
+              {((item.Gender && <h1>{item.Gender}</h1>) || (item.Type && <h1>{"Type: " + item.Type}</h1>))}
+              {((item.Age && <h1>{item.Age}</h1>) || (item.Season && <h1>{item.Season}</h1>))}
+            </div>
           )
       )}
-      <Link to="/Home/Requests">
-        <button className="back-button">Back</button>
-      </Link>
+      <Box sx={{ display: "flex", gap: 2, mt: 2 }}>
+        <Link to="/Home/Requests" style={{ textDecoration: 'none' }}>
+          <Button variant="contained" className="back-button">
+            Back
+          </Button>
+        </Link>
+        <Button variant="contained" className="donate-button">
+          Donate
+        </Button>
+      </Box>
     </div>
   );
 };
