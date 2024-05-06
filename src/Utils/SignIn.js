90a13c1df9e814dad117d4f4a624ebebadbf0129
yousefyasser <yousefyasser6@gmail.com>
2024-05-06 19:57:04 +0300

    Restyled signIn page

--- src/Utils/SignIn.js
+++ src/Utils/SignIn.js
@@ -13,6 +13,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // ICONS
@@ -153,85 +154,123 @@ export default function SignIn({ setActiveUser, setNavbarContent }) {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
+        <Grid
+          container
+          spacing={5}
+          style={{
             display: "flex",
-            flexDirection: "column",
+            justifyContent: "center",
             alignItems: "center",
+            marginTop: "100px",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              error={errorMsg === "Invalid Email"}
-            />
-            {errorMsg === "Invalid Email" && (
-              <span className="text-red-500">{errorMsg}</span>
-            )}
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              error={errorMsg === "Invalid Password"}
-            />
-            {errorMsg === "Invalid Password" && (
-              <>
-                <span className="text-red-500">{errorMsg}</span>
-                <br />
-              </>
-            )}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+          <Grid item xs={12} sm={5}>
+            <Paper
+              sx={{
+                padding: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link to="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link to="/Register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
+              <img
+                src={process.env.PUBLIC_URL + "/logo.png"}
+                alt="logo"
+                style={{ width: "100px", height: "auto" }}
+              />
+              <Typography variant="h5" component="h2">
+                Welcome to Fa3el 5eir
+              </Typography>
+              <Typography style={{ marginTop: "10px" }}>
+                We offer ease of communication between donors and organizations
+                to facilitate the donation process and helping out those in need
+                by fulfilling their requests either through clothes, food, toys,
+                medications, and so on ...
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <Paper
+              sx={{
+                padding: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  error={errorMsg === "Invalid Email"}
+                />
+                {errorMsg === "Invalid Email" && (
+                  <span className="text-red-500">{errorMsg}</span>
+                )}
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  error={errorMsg === "Invalid Password"}
+                />
+                {errorMsg === "Invalid Password" && (
+                  <>
+                    <span className="text-red-500">{errorMsg}</span>
+                    <br />
+                  </>
+                )}
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link to="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link to="/Register" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
       </Container>
     </ThemeProvider>
   );
