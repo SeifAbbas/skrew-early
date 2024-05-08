95de6ede48bb64f03ec6675d5e63d18564d07ee1
yousefyasser <yousefyasser6@gmail.com>
2024-05-08 20:18:29 +0300

    Added review donor submissions table to admin dashboard

--- src/Admin/AdminDashboard.js
+++ src/Admin/AdminDashboard.js
@@ -1,8 +1,48 @@
+import { Box, Toolbar, Container, Grid, Paper } from "@mui/material";
+
+import dummyData from "../dummyData.json";
+import DonorDetailsSubTable from "../Organization/MyRequests/DonorDetailsSubTable";
+
 const AdminDashboard = () => {
   return (
-    <>
-      <h1>hello from Admin Dashboard</h1>
-    </>
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Toolbar />
+      <Container maxWidth="xl" sx={{ mb: 4 }}>
+        <Grid container spacing={2} justifyContent={"center"}>
+          {/** DONORS SUBMISSIONS TABLE */}
+          <Grid item lg={6} md={12} sx={{ p: 2 }}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                boxShadow: 3,
+                borderRadius: "25px",
+              }}
+            >
+              <DonorDetailsSubTable
+                row={dummyData.FulfilledRequests[3]}
+                open={true}
+                title={"Review Donor Submissions"}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
